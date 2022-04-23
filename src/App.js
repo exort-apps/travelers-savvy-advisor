285c97caed00783624cafb47f72d9e0151f083d1
exort-apps <67579080+exort-apps@users.noreply.github.com>
2022-04-23 21:34:56 +0200

    temp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {CssBaseline, Grid} from '@material-ui/core';
+
+import { getPlacesData } from './api'
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+
 
 const App = () => {
 
+    const [places, setPlaces] = useState([]);
+
+    const [coordinates, setCoordinates] = useState({});
+    const [bounds, setBounds] = useState(null);
+
+    useEffect(() => {
+        getPlacesData()
+            .then((data) => {
+                console.log(data);
+                setPlaces(data);
+            })
+    }, [])
+
     return (
-        <div>
-            <h1>Hello World!</h1>
-        </div>
+        <>
+        <CssBaseline />
+        <Header />
+        <Grid container spacing={3} style={{width: '100%'}}>
+            <Grid item xs={12} md={4}>
+                <List />
+            </Grid>
+            <Grid item xs={12} md={8}>
+                <Map 
+                    setCoordinates={setCoordinates}
+                    setBounds={setBounds}
+                    coordinates={coordinates}
+                />
+            </Grid>
+        </Grid>
+        </>
     )
 }
 
---
+++ src/components/ObjectDetails/ObjectDetails.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ObjectDetails = ({place}) => {
+
+    return (
+        <h1>{place.name}</h1>
+    )
+}
+
+export default ObjectDetails;
